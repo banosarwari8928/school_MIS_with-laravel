5d3adfe697883d8097f48b0a02efb1a3e54623de
banosarwari8928 <banoosarwari8928@gmail.com>
2026-03-31 11:48:41 +0430

    worked on models

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     //
-   
+    protected $fillable = [
+        "firstName",
+        "lastName",
+        "email",
+        "phone",
+        "tazkira",
+        "gender"
+    ];
 }
