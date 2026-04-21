8ee778158d9130fa60f4277a004e8ae1fc4539fd
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-21 14:33:01 +0430

    seeder added

--- database/seeders/SchoolSeeder.php
+++ database/seeders/SchoolSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SchoolSeeder extends Seeder
 {
@@ -13,5 +14,9 @@ class SchoolSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table("schools")->insert([
+          "name"=>"Sharifi High School",
+          "whatsapp_link"=>"0790616105"  
+        ]);
     }
 }
