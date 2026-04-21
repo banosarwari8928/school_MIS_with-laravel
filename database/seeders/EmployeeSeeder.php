2548bb9835c68801374865e4cc790de7b27212d7
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-21 14:40:57 +0430

    seeder added

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EmployeeSeeder extends Seeder
 {
@@ -12,6 +13,13 @@ class EmployeeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+    DB::table("employees")->insert([
+        "firstName"=>"Murtaza",
+        "lastName"=>"Rezaie",
+        "tazkira"=>"123367975",
+        "phone"=>"0791168586",
+        "email"=>"mure@gmail.com"
+    ]); 
+    //
     }
 }
