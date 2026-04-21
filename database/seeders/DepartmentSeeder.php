e6319f798c5f82202e0ece1e66bc7e65ce05d423
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-21 14:36:31 +0430

    seeder added

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DepartmentSeeder extends Seeder
 {
@@ -13,5 +14,9 @@ class DepartmentSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table("departments")->insert([
+            "name"=>"Camputer",
+            "school_id"=>1,
+        ]);
     }
 }
