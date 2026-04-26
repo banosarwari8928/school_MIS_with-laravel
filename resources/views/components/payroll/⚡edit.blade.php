8bb12bfbf8238c297469886813d4d89fdccf68d8
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-26 14:51:00 +0430

    worked on  payroll's logic

--- resources/views/components/payroll/⚡edit.blade.php
+++ resources/views/components/payroll/⚡edit.blade.php
@@ -1,10 +1,20 @@
 <?php
 
+use App\Models\Payroll;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 new class extends Component
 {
     //
+     public $payroll;
+    public $id ;
+    public function update(Request $request , string $id){
+        $this->id = $id;
+    }
+    public function mount(){
+        $this->payroll = Payroll::findOrFail($this->id);
+    }
 };
 ?>
 
