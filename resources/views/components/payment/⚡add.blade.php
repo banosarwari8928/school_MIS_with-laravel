3b80055d7fae88fb3bb4d497eb6ac1373dd29f73
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-26 14:42:46 +0430

    worked on editing employee logic

--- resources/views/components/payment/⚡add.blade.php
+++ resources/views/components/payment/⚡add.blade.php
@@ -1,10 +1,30 @@
 <?php
 
+use App\Models\Payment;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 new class extends Component
 {
     //
+    
+    public $payment;
+
+    public function mount(){
+        $this->payment = new Payment();
+    }
+    public function create(Request $request){
+        $request->validate([
+        "payment_mount"=> "required|integer",
+        "emploee_id"=> "required|integer",
+        "payroll_id"=> "required|integer",
+        ]);
+        $this->payment->create([
+            "payment_mount"=> $request->payment_mount,
+        "emploee_id" => $request->emploee_id,
+        "payroll_id"=> $request->payroll_id,
+        ]);
+    }
 };
 ?>
 
