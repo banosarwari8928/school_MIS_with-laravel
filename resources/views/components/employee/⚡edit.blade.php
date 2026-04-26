09781186faeed13188653d888fb948aae7d56ad6
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-26 14:41:18 +0430

    worked on editing employee logic

--- resources/views/components/employee/⚡edit.blade.php
+++ resources/views/components/employee/⚡edit.blade.php
@@ -1,9 +1,19 @@
 <?php
 
+use App\Models\Employee;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 new class extends Component
 {
+     public $id ;
+    public $employeeData;
+    public function update(Request $request , string $id){
+        $this->id = $id ;
+    }
+    public function mount(){
+        $this->employeeData = Employee::findOrFail($this->id);
+    }
     //
 };
 ?>
