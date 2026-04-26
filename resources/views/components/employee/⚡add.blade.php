2cc7ade5f6cd922d6fbe1e1d60b99cb90a865871
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-26 14:40:34 +0430

    worked on editing employee logic

--- resources/views/components/employee/⚡add.blade.php
+++ resources/views/components/employee/⚡add.blade.php
@@ -1,10 +1,35 @@
 <?php
 
+use App\Models\Employee;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 new class extends Component
 {
     //
+     public $employee;
+     public function mount(){
+        $this->employee = new Employee();
+     }
+     public function Create(Request $request){
+        $request->validate([
+        "firstName" => "required|string|min:4",
+        "LastName"=> "required|string|min:6",
+        "tazkira"=> "required|integer",
+        "email" => "required|string",
+        "phone"=> "nullable|string|max:10",
+        "designation_id"=> "required|integer"
+        ]);
+        $this->employee->Create([
+        "firstName"=>$request->firstName,
+        "LastName"=> $request->lastName,
+        "tazkera"=> $request->tazkera,
+        "gender"=> $request->gender,
+        "email"=> $request->email,
+        "phone" => $request->phone,
+        "designation_id" =>$request->designation_id
+        ]);
+     }
 };
 ?>
 
