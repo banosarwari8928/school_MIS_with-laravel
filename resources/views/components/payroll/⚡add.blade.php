04bc9b0dc3bd3f0c04bc6832276588421d41bc7c
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-26 14:50:34 +0430

    worked on  payroll's logic

--- resources/views/components/payroll/⚡add.blade.php
+++ resources/views/components/payroll/⚡add.blade.php
@@ -1,10 +1,28 @@
 <?php
 
+use App\Models\Payroll;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public $payroll;
+    public function mount(){
+        $this->payroll = new Payroll();
+    }
+    public function create(Request $request){
+        $request->validate([
+             "year"=>"required|date",
+        "month"=> "required|date",
+        "school_id"=> "required|integer",
+        ]);
+        $this->payroll->create([
+            "Year"=> $request->year,
+            "month"=> $request->month,
+            "school_id"=> $request->school_id,
+        ]);
+    }
+
 };
 ?>
 
