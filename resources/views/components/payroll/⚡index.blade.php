152b410cb53f4aa84618a75e68dd59f8b23a189b
banosarwari8928 <banosarwari8928@gmail.com>
2026-04-26 14:51:19 +0430

    worked on  payroll's logic

--- resources/views/components/payroll/⚡index.blade.php
+++ resources/views/components/payroll/⚡index.blade.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Models\Payroll;
 use Livewire\Component;
 
 new class extends Component
 {
     //
+     public $payroll ;
+    public function mount(){
+        $this->payroll = Payroll::all();
+    }
 };
 ?>
 
